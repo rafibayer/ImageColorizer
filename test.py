93f36ac299a83e011150239ff423a5a387b885eb
Rafael Bayer <rafibayer7@gmail.com>
2019-08-14 15:08:43 -0700

    cleaning tests

--- test.py
+++ test.py
@@ -3,10 +3,12 @@ import matplotlib.pyplot as plt
 import matplotlib
 
 
+path = "seattle.jpg"
+out_name = "seattle_color.jpg"
+img_size = 1024
 
-filename = "dude_color.jpg"
-image = predictor.colorizeImg("dude.jpg", 1024)
-matplotlib.image.imsave("output/{}".format(filename), image)
+image = predictor.colorizeImg("images/{}".format(path), img_size)
+matplotlib.image.imsave("output/{}".format(out_name), image)
 
 
 plt.imshow(image)
