6a06c8e88f59cbe8229d4e892aa558756897cf44
Rafael Bayer <rafibayer7@gmail.com>
2019-08-16 17:34:19 -0700

    working on app interface

--- app.py
+++ app.py
@@ -1,19 +1,67 @@
 from tkinter import *
 from tkinter import ttk
+from PIL import ImageTk,Image
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
+from predictor import Predictor
+
+IMAGE_SIZE = 500
+
+pred = Predictor()
+
+#original_image = ImageTk.PhotoImage(load_img("blank.jpg", target_size = (IMAGE_SIZE, IMAGE_SIZE)))
+original_image = ImageTk.PhotoImage(Image.open("blank.jpg"))
+color_image = ImageTk.PhotoImage(Image.open("blank.jpg"))
+
+def colorizeImage(path, resolution):
+
+    print("colorizing...")
+    color = pred.colorizeImg(path, resolution)
+
+    original_image = ImageTk.PhotoImage(load_img(path, target_size = (IMAGE_SIZE, IMAGE_SIZE)))
+    canvasOrig.image = original_image 
+
+    color_image = ImageTk.PhotoImage(Image.fromarray((color * 255).astype("uint8")).resize((IMAGE_SIZE, IMAGE_SIZE)))
+    canvasCol.image = color_image
+
+    print("done!")
+
 
 root = Tk()
 
-frame = Frame(root)
+controls = Frame(root, height=600, width=300)
+controls.grid(row=0, column=0, sticky=NW)
 
-labelText = StringVar()
+Label(controls, text="Options:").grid(row=0, column=0, sticky=W)
 
-label = Label(frame, textvariable=labelText)
-button = Button(frame, text='Click')
+Label(controls, text="Target Resolution:").grid(row=1, column=0, sticky=W)
+resolutionEntry = Entry(controls)
+resolutionEntry.grid(row=1, column=1, sticky=W)
 
-labelText.set("I am a label")
+chooseButton = Button(controls, text="Choose Image")
+chooseButton.grid(row=2, column=0, sticky=W)
 
-label.pack()
-button.pack()
-frame.pack()
+saveButton = Button(controls, text="Save Image")
+saveButton.grid(row=2, column=1, sticky=W)
+
+###############################################
+
+images = Frame(root)
+images.grid(row=0, column = 1, sticky=E) 
+
+Label(images, text="Original").grid(row=0, column=0, sticky=W)
+canvasOrig = Label(images, height = IMAGE_SIZE, width = IMAGE_SIZE)
+canvasOrig.grid(row=1, column=0)
+canvasOrig.image = original_image 
+
+
+
+Label(images, text="Colorized").grid(row=0, column=1, sticky=W)
+canvasCol = Label(images, height = IMAGE_SIZE, width = IMAGE_SIZE)
+canvasCol.grid(row=1, column=1)
+canvasCol.image = color_image 
+
+
+#colorizeImage("images/nyc.jpg", 512)
+  
 
 root.mainloop()
--- predictor.py
+++ predictor.py
@@ -3,86 +3,88 @@ from tensorflow.keras.preprocessing.image import img_to_array, load_img
 from skimage.color import lab2rgb, rgb2lab
 import numpy as np
 import matplotlib.pyplot as plt
-
-
-# Model reconstruction from JSON file
-with open('model_arch.json', 'r') as f:
-    model = model_from_json(f.read())
-
-# Load weights into the model
-model.load_weights('model.h5')
-
-
 import math
 
-# load the l component of an image (LAB colorspace)
-def load_lab_bw(path, size):
-  image = img_to_array(load_img(path, target_size=size)) / 255 #open image as array and normalize values [0, 1]
-  lab_image = rgb2lab(image)
-  lab_image = (lab_image + [0, 128, 128]) / [100, 255, 255] # normalize LAB
-
-  bw = lab_image[:,:,0] # Take just the L component, black and white image
-  return bw
-
-
-# splice a square bw image of im_size^2 into square of split_size^2
-def splice_square_image(bw_image, im_size, split_size):
-  dim = im_size / split_size # calculate the number of chunks for width and height
-  if dim != int(dim):
-    raise Exception("Error, please use a square number for tiles")
+class Predictor:
   
-  dim = int(dim)
-  # size of chunk
-  C = split_size
-  
-  bw_chunks = [] # holds each chunk/tile 
-  
-  for x in range(dim):
-    for y in range(dim):
-      bw_chunks.append(bw_image[x*C : x*C + C, y*C : y*C + C]) # slice the image into chunks and append onto list
+  def __init__(self):
+    # Model reconstruction from JSON file
+    with open('model_arch.json', 'r') as f:
+        self.model = model_from_json(f.read())
 
-
-  bw_chunks = np.expand_dims(np.array(bw_chunks), 3) # add an extra dimension to satisfy model input shape
-  return bw_chunks
-
-# predicts the color in a set of images, use to predict color for each chunk
-def predict_color(images):
-    return model.predict(images)
+    # Load weights into the model
+    self.model.load_weights('model.h5')
 
 
 
-# reconstruct an image of im_size^2 split into split_size^2 chunks, 
-# use bw image for l component of LAB and color as color predictions
-def reconstruct_img(im_size, split_size, bw, color):
-  # calculate sidelength
-  dim = (im_size / split_size)
-  if dim != int(dim):
-    raise Exception("Error, please use a square number for tiles, used: {}".format(dim))
-  
-  dim = int(dim)
-  j = 0 # chunk index variable
-  C = split_size 
+  # load the l component of an image (LAB colorspace)
+  def load_lab_bw(self, path, size):
+    image = img_to_array(load_img(path, target_size=size)) / 255 #open image as array and normalize values [0, 1]
+    lab_image = rgb2lab(image)
+    lab_image = (lab_image + [0, 128, 128]) / [100, 255, 255] # normalize LAB
 
-  result = np.zeros((im_size, im_size, 3)) # reconstructed output image
+    bw = lab_image[:,:,0] # Take just the L component, black and white image
+    return bw
 
-  for x in range(dim):
-    for y in range(dim):
-      out = np.zeros((split_size, split_size, 3))
 
-      out[:,:,0] = bw[j][:,:,0] # L component from original black and white image
-      out[:,:,1:] = color[j] # A and B components predicted by neural network
+  # splice a square bw image of im_size^2 into square of split_size^2
+  def splice_square_image(self, bw_image, im_size, split_size):
+    dim = im_size / split_size # calculate the number of chunks for width and height
+    if dim != int(dim):
+      raise Exception("Error, please use a square number for tiles")
+    
+    dim = int(dim)
+    # size of chunk
+    C = split_size
+    
+    bw_chunks = [] # holds each chunk/tile 
+    
+    for x in range(dim):
+      for y in range(dim):
+        bw_chunks.append(bw_image[x*C : x*C + C, y*C : y*C + C]) # slice the image into chunks and append onto list
 
-      out = (out * [100, 255, 255]) - [0, 128, 128] # denormalizing
-      out = lab2rgb(out) # converting chunk back to RGB
 
-      result[x*C: x*C + C, y*C: y*C + C,:] = out # placing reconstructed chunk into the final image
-      j += 1
-      
-  return result
+    bw_chunks = np.expand_dims(np.array(bw_chunks), 3) # add an extra dimension to satisfy model input shape
+    return bw_chunks
 
-# single function that takes in an image path and size, and returns a colorized image
-def colorizeImg(path, img_size):
-    image = load_lab_bw(path, (img_size,img_size))
-    bw_chunks = splice_square_image(image, img_size, 256)
-    pred = predict_color(bw_chunks)
-    return reconstruct_img(img_size, 256, bw_chunks, pred)
+  # predicts the color in a set of images, use to predict color for each chunk
+  def predict_color(self, images):
+      return self.model.predict(images)
+
+
+
+  # reconstruct an image of im_size^2 split into split_size^2 chunks, 
+  # use bw image for l component of LAB and color as color predictions
+  def reconstruct_img(self, im_size, split_size, bw, color):
+    # calculate sidelength
+    dim = (im_size / split_size)
+    if dim != int(dim):
+      raise Exception("Error, please use a square number for tiles, used: {}".format(dim))
+    
+    dim = int(dim)
+    j = 0 # chunk index variable
+    C = split_size 
+
+    result = np.zeros((im_size, im_size, 3)) # reconstructed output image
+
+    for x in range(dim):
+      for y in range(dim):
+        out = np.zeros((split_size, split_size, 3))
+
+        out[:,:,0] = bw[j][:,:,0] # L component from original black and white image
+        out[:,:,1:] = color[j] # A and B components predicted by neural network
+
+        out = (out * [100, 255, 255]) - [0, 128, 128] # denormalizing
+        out = lab2rgb(out) # converting chunk back to RGB
+
+        result[x*C: x*C + C, y*C: y*C + C,:] = out # placing reconstructed chunk into the final image
+        j += 1
+        
+    return result
+
+  # single function that takes in an image path and size, and returns a colorized image
+  def colorizeImg(self, path, img_size):
+      image = self.load_lab_bw(path, (img_size,img_size))
+      bw_chunks = self.splice_square_image(image, img_size, 256)
+      pred = self.predict_color(bw_chunks)
+      return self.reconstruct_img(img_size, 256, bw_chunks, pred)
